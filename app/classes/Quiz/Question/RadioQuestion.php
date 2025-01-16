743598c54a74269a3f021dffcc1d080f557c0389
baptiste-mignan <baptiste.mignan@gmail.com>
2025-01-16 21:44:22 +0100

    classes Radio et RadioQuestion

--- app/classes/Quiz/Question/RadioQuestion.php
+++ app/classes/Quiz/Question/RadioQuestion.php
@@ -1,6 +1,35 @@
 <?php
 declare(strict_types=1);
 
+namespace Quiz\Question;
+
+use Quiz\GenericQuestion;
+use Input\Type\Radio;
+
+final class RadioQuestion extends GenericQuestion{
+
+    private array $choices;
+
+    public function __construct(string $name, string $type, string $text, $answer, float $point, array $choices){
+        parent::__construct($name, $type, $text, $answer, $point);
+        $this->choices = $choices;
+    }
+
+    public function render(): string{
+        $intitule = sprintf('<p>%s</p>', $this->text);
+        $radio = '<div>'."\n";
+        foreach ($this->choices as $choice){
+            $radio .= new Radio($this->name, false, $defaultValue = $choice["value"]).sprintf('<label for='.$choice["value"].'>%s</label>', $choice["text"])."\n";
+        }
+        $radio .= '<div>'."\n";
+        return $intitule."\n".$radio."\n";
+    }
+
+    public function __tostring(): string{
+        return $this->render();
+    }
+}
+
 //      array(
 //         "name" => "cheval",
 //         "type" => "radio",
--- app/index.php
+++ app/index.php
@@ -7,6 +7,7 @@ use Form\Form;
 use Input\Type\Checkbox;
 use Quiz\Question\TextQuestion;
 use Quiz\Question\CheckboxQuestion;
+use Quiz\Question\RadioQuestion;
 
 $form = new Form();
 
@@ -15,9 +16,12 @@ $question1 = new TextQuestion("ultime", "text", "Quelle est la réponse ultime?"
 $question2 = new CheckboxQuestion("drapeau", "checkbox", "Quelles sont les couleurs du drapeau français?", ["bleu", "blanc", "rouge"], 3,
 [array("text" => "Bleu", "value" => "bleu"), array("text" => "Blanc", "value" => "blanc"), array("text" => "Vert", "value" => "vert"), array("text" => "Jaune", "value" => "jaune"), array("text" => "Rouge", "value" => "rouge")]);
 
+$question3 = new RadioQuestion("cheval", "radio", "Quelle est la couleur du cheval blanc d'Henri IV?", "blanc", 2,
+[array("text" => "Bleu", "value" => "bleu"),array("text" => "Blanc", "value" => "blanc"),array("text" => "Rouge", "value" => "rouge")]);
 
 
 $form->add($question1);
 $form->add($question2);
+$form->add($question3);
 
 echo $form->render();
